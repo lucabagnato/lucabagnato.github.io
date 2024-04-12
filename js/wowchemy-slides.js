7bf883494c94aa50dbf54241021dc2598251f451
lucabagnato <lucabgn@gmail.com>
2024-04-12 12:33:23 +0200

    “newdb”

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/Users/lbagna/Dropbox/Website/website/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
+  // ns-hugo:/Users/lbagna/DEMM - UNIMI Dropbox/Luca Bagnato/Website/website/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
